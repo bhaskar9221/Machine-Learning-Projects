7e801031d14215e442b26080f7cb275608e7369e
Bhaskar Kumar Mondal <75377209+bhaskar9221@users.noreply.github.com>
2024-04-13 23:33:51 +0530

    Update app.py

--- Stock-Price-Forecasting/app.py
+++ Stock-Price-Forecasting/app.py
@@ -10,7 +10,7 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Stock Forecast App')
+st.title('Stock Forecasting App')
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
