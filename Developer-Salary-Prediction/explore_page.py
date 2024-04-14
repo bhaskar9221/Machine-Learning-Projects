1d2c6db28ff87aa7d1a4b51b5eb2f14a8c28aa33
Bhaskar Kumar Mondal <75377209+bhaskar9221@users.noreply.github.com>
2024-04-15 00:24:24 +0530

    Update explore_page.py

--- Developer-Salary-Prediction/explore_page.py
+++ Developer-Salary-Prediction/explore_page.py
@@ -32,7 +32,7 @@ def clean_education(x):
 
 @st.cache
 def load_data():
-    df = pd.read_csv("survey_results_public.csv")
+    df = pd.read_csv("Developer-Salary-Prediction/survey_results_public.csv")
     df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedComp"]]
     df = df[df["ConvertedComp"].notnull()]
     df = df.dropna()
